a8c5ba69c3ffa2aecde0e7b3290fc0bf287482da
M_Dev_Pro <mdevpro.2020@gmail.com>
2020-06-18 21:06:32 +0430

    tested => FormView, refactored => FormView

--- views/FormView.py
+++ views/FormView.py
@@ -1,4 +1,5 @@
-from tkinter import (Label, Entry, Tk,
+from tkinter import (Label, Entry,
+                     Toplevel,
                      Button, Frame)
 
 from tkinter import StringVar, IntVar
@@ -18,10 +19,10 @@ class ContactView:
     def get_attributes():
         return 'Name', 'Last Name', 'Phone Number', 'Gender'
 
-    @property
-    def informaion(self):
-        return {'first_name':self.name.get(), 'last_name': self.last_name.get(),
-                'phone_number': self.phone_number.get(), 'gender': self.gender.get(), 'id':self.id.get()}
+    # @property
+    # def information(self):
+    #     return {'first_name': self.name.get(), 'last_name': self.last_name.get(),
+    #             'phone_number': self.phone_number.get(), 'gender': self.gender.get(), 'id': self.id.get()}
 
     def set_values(self, **kwargs):
         self.name.set(kwargs['Firstname'])
@@ -31,71 +32,79 @@ class ContactView:
         self.id.set(int(kwargs['id']))
 
 
-class FormView(Frame):
-    def __init__(self, window, title):
-        super().__init__(window)
-        self.window = window
-        self.window.resizable(width=False, height=False)
-        self.window.configure(background='#96C3CE')
+class FormView(Toplevel):
+    OPTIONS = {'font': ('bold', 11)}
+    def __init__(self, parent, title):
+        super().__init__(parent)
+        self.resizable(width = False, height = False)
+        self.configure(background = '#96C3CE')
+        self.title(f'{title}')
+        self.geometry('500x500')
+
         self.contact = ContactView()
-        self.window.title(f'{title}')
-        self.window.geometry('500x500')
-        self.window_title = Label(self.window, text='Contact Form',
-                                  width=20, font=("bold", 20))
-        self.window_title.place(x=90, y=53)
+        self.window_title = Label(self, text = 'Contact Form',
+                                  width = 20, font = ("bold", 20))
+        self.window_title.place(x = 90, y = 53)
+
         self.create_labels()
         self.create_entries()
         self.create_combobox()
         self.create_buttons()
 
+
     def create_entries(self):
-        self.name_entry = Entry(self.window, textvariable = self.contact.name,
-                                text=self.contact.name)
+        self.name_entry = Entry(self, textvariable = self.contact.name,
+                                text = self.contact.name, **self.OPTIONS)
         self.name_entry.place(x = 240, y = 130)
 
-        self.last_name_entry = Entry(self.window,
+        self.last_name_entry = Entry(self,
                                      textvariable = self.contact.last_name,
-                                     text=self.contact.last_name)
+                                     text = self.contact.last_name, **self.OPTIONS)
         self.last_name_entry.place(x = 240, y = 180)
 
-        self.phone_nubmer_entry = Entry(self.window,
+        self.phone_nubmer_entry = Entry(self,
                                         textvariable = self.contact.phone_number,
-                                        text=self.contact.phone_number)
+                                        text = self.contact.phone_number, **self.OPTIONS)
         self.phone_nubmer_entry.place(x = 240, y = 230)
 
+
     def create_labels(self):
-        frame = Frame(self.window, width=100, height=140, bg='#96C3CE')
+        frame = Frame(self, width = 100, height = 140, bg = '#96C3CE')
         for index, item in enumerate(self.contact.get_attributes()):
-            lbl = Label(frame, text=f"{item}: ", width=14,
-                        font=("bold", 11), bg='#96C3CE', anchor='w')
-            lbl.grid(row=index, column=0, pady=13.5)
-        frame.place(x=120, y=115)
+            lbl = Label(frame, text = f"{item}: ", width = 14,
+                        bg = '#96C3CE', anchor = 'w', **self.OPTIONS)
+            lbl.grid(row = index, column = 0, pady = 13.5)
+        frame.place(x = 120, y = 115)
+
 
     def create_combobox(self):
         self.contact_gender = ['Female', 'Male']
-        self.gender_options = Combobox(self.window, values=self.contact_gender)
-        self.gender_options.config(width=17)
-        self.gender_options.current(0)
-        self.gender_options.place(x = 238, y = 280)
+        self.gender = Combobox(self, values = self.contact_gender, **self.OPTIONS)
+        self.gender.config(width = 17)
+        self.gender.current(0)
+        self.gender.place(x = 238, y = 280)
+
 
     def create_buttons(self):
-        self.submit_btn = Button(self.window, text = 'Submit',
+        self.submit_btn = Button(self, text = 'Submit',
                                  width = 20, bg = '#58A4B0',
-                                 fg = '#020100', font = ('bold', 11))
+                                 fg = '#020100', **self.OPTIONS)
         self.submit_btn.place(x = 255, y = 350)
 
-        Button(self.window, text='Cancell', width=20,
-               bg='#D64933', command=self.window.destroy,
-               fg='#020100', font=('bold', 11)).place(x=55, y=350)
+        Button(self, text = 'Cancell', width = 20,
+               bg = '#D64933', command = self.destroy,
+               fg = '#020100', **self.OPTIONS).place(x = 55, y = 350)
+
 
     def insert_item(self, **kwargs):
         self.contact.set_values(**kwargs)
         self.name_entry.insert(0, f'{self.contact.name.get()}')
         self.last_name_entry.insert(0, f"{self.contact.last_name.get()}")
         self.phone_nubmer_entry.insert(0, f"{self.contact.phone_number.get()}")
-        self.gender_options.current(self.contact_gender.index(kwargs['Gender']))
+        self.gender.current(self.contact_gender.index(kwargs['Gender']))
 
 
-# app = Tk()
-# obj = FormView('test', app)
-# app.mainloop()
+    @property
+    def get_entries_values(self):
+        return {'first_name': self.name_entry.get(), 'last_name': self.last_name_entry.get(),
+                'phone_number': self.phone_nubmer_entry.get(), 'gender': self.gender.get()}
